311df4a13ddc70c16d33372336a87ccfbe2bd9bf
Amulya Manoj <amulyamanoj@berkeley.edu>
2025-05-18 22:04:29 -0700

    Initial Commit

---
+++ GitPracticeTests/GitPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeTests.swift
+//  GitPracticeTests
+//
+//  Created by Scholar on 5/18/25.
+//
+
+import Testing
+@testable import GitPractice
+
+struct GitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
